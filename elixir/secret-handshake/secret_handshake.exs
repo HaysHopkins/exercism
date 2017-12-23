6b24f2d390c0936d2375d04235685a20c28c0362
hays.hopkins@salesloft.com <hays.hopkins@salesloft.com>
2017-12-23 18:04:07 -0500

    Complete elixir secret handshake

---
+++ elixir/secret-handshake/secret_handshake.exs
@@ -0,0 +1,32 @@
+defmodule SecretHandshake do
+  @doc """
+  Determine the actions of a secret handshake based on the binary
+  representation of the given `code`.
+
+  If the following bits are set, include the corresponding action in your list
+  of commands, in order from lowest to highest.
+
+  1 = wink
+  10 = double blink
+  100 = close your eyes
+  1000 = jump
+
+  10000 = Reverse the order of the operations in the secret handshake
+  """
+  @spec commands(code :: integer) :: list(String.t())
+  def commands(code) do
+    Integer.digits(code, 2)
+    |> Enum.reverse
+    |> issue_commands
+  end
+
+  def issue_commands(list), do: _issue_commands(list, [], 0)
+  defp _issue_commands([], acc, _pos),    do: acc
+  defp _issue_commands([1|t], acc, 0),    do: _issue_commands(t, acc++["wink"], 1)
+  defp _issue_commands([1|t], acc, 1),    do: _issue_commands(t, acc++["double blink"], 2)
+  defp _issue_commands([1|t], acc, 2),    do: _issue_commands(t, acc++["close your eyes"], 3)
+  defp _issue_commands([1|t], acc, 3),    do: _issue_commands(t, acc++["jump"], 4)
+  defp _issue_commands([1|t], acc, 4),    do: _issue_commands(t, Enum.reverse(acc), 5)
+  defp _issue_commands([_h|t], acc, pos), do: _issue_commands(t, acc, pos+1)
+end
+
