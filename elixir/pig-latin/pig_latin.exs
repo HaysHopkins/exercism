11f6da5f8bf78dbf844eef46718941456eb3b812
hays.hopkins@salesloft.com <hays.hopkins@salesloft.com>
2017-12-29 16:04:04 -0500

    Finish pig latin implementation

--- elixir/pig-latin/pig_latin.exs
+++ elixir/pig-latin/pig_latin.exs
@@ -13,9 +13,31 @@ defmodule PigLatin do
 
   Some groups are treated like vowels, including "yt" and "xr".
   """
+  @vowels ["a", "e", "i", "o", "u", "yt", "xr"]
+  @consonant_groups ["thr", "th", "str", "squ", "sch", "pl", "qu", "ch"]
+  @matching_prefixes @vowels ++ @consonant_groups
+
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
-    # moving to next challenge
+    String.split(phrase, " ")
+    |> Enum.map(&pig_latinize/1)
+    |> Enum.join(" ")
   end
+
+  defp pig_latinize(word) do
+    find_prefix(word)
+    |> remove_prefix(word)
+    |> add_suffix
+  end
+
+  defp find_prefix(word) do
+    Enum.find(@matching_prefixes, split(word), fn(match) -> String.starts_with?(word, match) end)
+  end
+  defp split(word), do: word |> String.split(~r/[aieou]/) |> Enum.at(0)
+
+  defp remove_prefix(prefix, word) when prefix in @vowels, do: {"", word}
+  defp remove_prefix(prefix, word), do: String.split_at(word, String.length(prefix))
+
+  defp add_suffix({prefix, word}), do: word <> prefix <> "ay"
 end
 
