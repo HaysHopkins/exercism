c82dc87943fa3f9b8aed3c4979819ba2f84e4747
hays.hopkins@salesloft.com <hays.hopkins@salesloft.com>
2017-12-29 16:27:49 -0500

    Implement space age

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -7,7 +7,13 @@ defmodule SpaceAge do
   aged on 'planet'.
   """
   @spec age_on(planet, pos_integer) :: float
-  def age_on(planet, seconds) do
-
-  end
+  def age_on(planet, seconds), do: _age_on(planet, seconds)
+  defp _age_on(planet, seconds) when planet == :earth,   do: seconds / 31557600
+  defp _age_on(planet, seconds) when planet == :mercury, do: _age_on(:earth, seconds) / 0.2408467
+  defp _age_on(planet, seconds) when planet == :venus,   do: _age_on(:earth, seconds) / 0.61519726
+  defp _age_on(planet, seconds) when planet == :mars,    do: _age_on(:earth, seconds) / 1.8808158
+  defp _age_on(planet, seconds) when planet == :jupiter, do: _age_on(:earth, seconds) / 11.862615
+  defp _age_on(planet, seconds) when planet == :saturn,  do: _age_on(:earth, seconds) / 29.447498
+  defp _age_on(planet, seconds) when planet == :uranus,  do: _age_on(:earth, seconds) / 84.016846
+  defp _age_on(planet, seconds) when planet == :neptune, do: _age_on(:earth, seconds) / 164.79132
 end
