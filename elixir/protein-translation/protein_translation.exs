485718bca6368bfc307521479bf8a6729029af72
hays.hopkins@salesloft.com <hays.hopkins@salesloft.com>
2017-12-29 16:19:46 -0500

    Iterate on protein translation

--- elixir/protein-translation/protein_translation.exs
+++ elixir/protein-translation/protein_translation.exs
@@ -1,4 +1,23 @@
 defmodule ProteinTranslation do
+  @proteins %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
@@ -6,14 +25,14 @@ defmodule ProteinTranslation do
   def of_rna(rna) do
     {segment, strand} = String.split_at(rna, 3)
     {status, codon} = of_codon(segment)
-    _format_result(_of_rna(status, codon, strand, []))
+    _of_rna(status, codon, strand, []) |> _format_result
   end
 
-  defp _of_rna(status, _codon, _strand, _acc) when status == :error,   do: "invalid RNA"
-  defp _of_rna(_status, codon, strand, acc)   when codon == "STOP",    do: Enum.reverse acc
-  defp _of_rna(_status, codon, strand, acc)   when strand == "",       do: Enum.reverse [codon|acc]
-  defp _of_rna(_status, codon, strand, _acc)  when length(strand) < 3, do: "invalid RNA"
-  defp _of_rna(_status, codon, strand, acc) do
+  defp _of_rna( status, _codon, _strand, _acc) when status == :error,   do: "invalid RNA"
+  defp _of_rna(_status,  codon,  strand,  acc) when codon == "STOP",    do: Enum.reverse acc
+  defp _of_rna(_status,  codon,  strand,  acc) when strand == "",       do: Enum.reverse [codon|acc]
+  defp _of_rna(_status,  codon,  strand, _acc) when length(strand) < 3, do: "invalid RNA"
+  defp _of_rna(_status,  codon,  strand,  acc) do
     {segment, next_strand} = String.split_at(strand, 3)
     {next_status, next_codon} = of_codon(segment)
     _of_rna(next_status, next_codon, next_strand, [codon|acc])
@@ -44,15 +63,12 @@ defmodule ProteinTranslation do
   UGA -> STOP
   """
   @spec of_codon(String.t()) :: { atom, String.t() }
-  def of_codon(codon) when codon in ["UGU", "UGC", "UUA", "UUG", "AUG", "UUU", "UUC", "UCU", "UCC", "UCA", "UCG", "UGG", "UAU", "UAC", "UAA", "UAG", "UGA"], do: {:ok, _of_codon(codon)}
-  def of_codon(_codon),                                              do: {:error, "invalid codon"}
-  defp _of_codon(codon) when codon in ["UGU", "UGC"],               do: "Cysteine"
-  defp _of_codon(codon) when codon in ["UUA", "UUG"],               do: "Leucine"
-  defp _of_codon(codon) when codon in ["AUG"],                      do: "Methionine"
-  defp _of_codon(codon) when codon in ["UUU", "UUC"],               do: "Phenylalanine"
-  defp _of_codon(codon) when codon in ["UCU", "UCC", "UCA", "UCG"], do: "Serine"
-  defp _of_codon(codon) when codon in ["UGG"],                      do: "Tryptophan"
-  defp _of_codon(codon) when codon in ["UAU", "UAC"],               do: "Tyrosine"
-  defp _of_codon(codon) when codon in ["UAA", "UAG", "UGA"],        do: "STOP"
+  def of_codon(codon) do
+    protein = @proteins[codon]
+    case protein do
+      nil -> {:error, "invalid codon"}
+      _   -> {:ok, protein}
+    end
+  end
 end
 
